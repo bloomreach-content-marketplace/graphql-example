f321a8880f98ce264f78e201f817505bbddbef1a
Kenan Salic <kenan.salic@bloomreach.com>
2023-10-24 09:59:24 +0200

    update

---
+++ client/src/generated/graphql.ts
@@ -0,0 +1,82 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  JSON: any;
+};
+
+export type Component = {
+  content?: Maybe<Scalars['JSON']>;
+  name: Scalars['String'];
+  properties?: Maybe<Scalars['JSON']>;
+};
+
+export type Container = {
+  components?: Maybe<Array<Maybe<Component>>>;
+  name: Scalars['String'];
+};
+
+export type Image = {
+  id: Scalars['String'];
+  src: Scalars['String'];
+};
+
+export type Menu = {
+  items?: Maybe<Array<Maybe<MenuItem>>>;
+  name: Scalars['String'];
+};
+
+export type MenuItem = {
+  expanded: Scalars['Boolean'];
+  href?: Maybe<Scalars['String']>;
+  items?: Maybe<Array<Maybe<MenuItem>>>;
+  name: Scalars['String'];
+  selected: Scalars['Boolean'];
+};
+
+export type Page = {
+  channel?: Maybe<Scalars['JSON']>;
+  containers?: Maybe<Array<Maybe<Container>>>;
+  data?: Maybe<Scalars['JSON']>;
+  menus?: Maybe<Array<Maybe<Menu>>>;
+  model?: Maybe<Scalars['JSON']>;
+  name: Scalars['String'];
+  path: Scalars['String'];
+  preview: Scalars['Boolean'];
+};
+
+export type Query = {
+  page?: Maybe<Page>;
+};
+
+
+export type QueryPageArgs = {
+  channel: Scalars['String'];
+  environment: Scalars['String'];
+  path: Scalars['String'];
+  segments?: InputMaybe<Array<InputMaybe<Scalars['String']>>>;
+  token?: InputMaybe<Scalars['String']>;
+};
+
+export type PageQueryVariables = Exact<{
+  environment: Scalars['String'];
+  channel: Scalars['String'];
+  path: Scalars['String'];
+  token?: InputMaybe<Scalars['String']>;
+}>;
+
+
+export type PageQuery = { page?: { name: string, data?: any | null, containers?: Array<{ name: string, components?: Array<{ name: string, content?: any | null, properties?: any | null } | null> | null } | null> | null, menus?: Array<{ name: string, items?: Array<{ href?: string | null, name: string } | null> | null } | null> | null } | null };
+
+
+export const PageDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Page"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"environment"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"channel"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"path"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"token"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"page"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"environment"},"value":{"kind":"Variable","name":{"kind":"Name","value":"environment"}}},{"kind":"Argument","name":{"kind":"Name","value":"channel"},"value":{"kind":"Variable","name":{"kind":"Name","value":"channel"}}},{"kind":"Argument","name":{"kind":"Name","value":"path"},"value":{"kind":"Variable","name":{"kind":"Name","value":"path"}}},{"kind":"Argument","name":{"kind":"Name","value":"token"},"value":{"kind":"Variable","name":{"kind":"Name","value":"token"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"data"}},{"kind":"Field","name":{"kind":"Name","value":"containers"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"components"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"content"}},{"kind":"Field","name":{"kind":"Name","value":"properties"}}]}}]}},{"kind":"Field","name":{"kind":"Name","value":"menus"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"items"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"href"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]}}]} as unknown as DocumentNode<PageQuery, PageQueryVariables>;
---
+++ server/src/generated/schema.ts
@@ -0,0 +1,232 @@
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  JSON: any;
+};
+
+export type Component = {
+  content?: Maybe<Scalars['JSON']>;
+  name: Scalars['String'];
+  properties?: Maybe<Scalars['JSON']>;
+};
+
+export type Container = {
+  components?: Maybe<Array<Maybe<Component>>>;
+  name: Scalars['String'];
+};
+
+export type Image = {
+  id: Scalars['String'];
+  src: Scalars['String'];
+};
+
+export type Menu = {
+  items?: Maybe<Array<Maybe<MenuItem>>>;
+  name: Scalars['String'];
+};
+
+export type MenuItem = {
+  expanded: Scalars['Boolean'];
+  href?: Maybe<Scalars['String']>;
+  items?: Maybe<Array<Maybe<MenuItem>>>;
+  name: Scalars['String'];
+  selected: Scalars['Boolean'];
+};
+
+export type Page = {
+  channel?: Maybe<Scalars['JSON']>;
+  containers?: Maybe<Array<Maybe<Container>>>;
+  data?: Maybe<Scalars['JSON']>;
+  menus?: Maybe<Array<Maybe<Menu>>>;
+  model?: Maybe<Scalars['JSON']>;
+  name: Scalars['String'];
+  path: Scalars['String'];
+  preview: Scalars['Boolean'];
+};
+
+export type Query = {
+  page?: Maybe<Page>;
+};
+
+
+export type QueryPageArgs = {
+  channel: Scalars['String'];
+  environment: Scalars['String'];
+  path: Scalars['String'];
+  segments?: InputMaybe<Array<InputMaybe<Scalars['String']>>>;
+  token?: InputMaybe<Scalars['String']>;
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  Component: ResolverTypeWrapper<Component>;
+  Container: ResolverTypeWrapper<Container>;
+  Image: ResolverTypeWrapper<Image>;
+  JSON: ResolverTypeWrapper<Scalars['JSON']>;
+  Menu: ResolverTypeWrapper<Menu>;
+  MenuItem: ResolverTypeWrapper<MenuItem>;
+  Page: ResolverTypeWrapper<Page>;
+  Query: ResolverTypeWrapper<{}>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Boolean: Scalars['Boolean'];
+  Component: Component;
+  Container: Container;
+  Image: Image;
+  JSON: Scalars['JSON'];
+  Menu: Menu;
+  MenuItem: MenuItem;
+  Page: Page;
+  Query: {};
+  String: Scalars['String'];
+};
+
+export type ComponentResolvers<ContextType = any, ParentType extends ResolversParentTypes['Component'] = ResolversParentTypes['Component']> = {
+  content?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  properties?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type ContainerResolvers<ContextType = any, ParentType extends ResolversParentTypes['Container'] = ResolversParentTypes['Container']> = {
+  components?: Resolver<Maybe<Array<Maybe<ResolversTypes['Component']>>>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type ImageResolvers<ContextType = any, ParentType extends ResolversParentTypes['Image'] = ResolversParentTypes['Image']> = {
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  src?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export interface JsonScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['JSON'], any> {
+  name: 'JSON';
+}
+
+export type MenuResolvers<ContextType = any, ParentType extends ResolversParentTypes['Menu'] = ResolversParentTypes['Menu']> = {
+  items?: Resolver<Maybe<Array<Maybe<ResolversTypes['MenuItem']>>>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type MenuItemResolvers<ContextType = any, ParentType extends ResolversParentTypes['MenuItem'] = ResolversParentTypes['MenuItem']> = {
+  expanded?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  href?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  items?: Resolver<Maybe<Array<Maybe<ResolversTypes['MenuItem']>>>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  selected?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type PageResolvers<ContextType = any, ParentType extends ResolversParentTypes['Page'] = ResolversParentTypes['Page']> = {
+  channel?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
+  containers?: Resolver<Maybe<Array<Maybe<ResolversTypes['Container']>>>, ParentType, ContextType>;
+  data?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
+  menus?: Resolver<Maybe<Array<Maybe<ResolversTypes['Menu']>>>, ParentType, ContextType>;
+  model?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  path?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  preview?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  page?: Resolver<Maybe<ResolversTypes['Page']>, ParentType, ContextType, RequireFields<QueryPageArgs, 'channel' | 'environment' | 'path'>>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Component?: ComponentResolvers<ContextType>;
+  Container?: ContainerResolvers<ContextType>;
+  Image?: ImageResolvers<ContextType>;
+  JSON?: GraphQLScalarType;
+  Menu?: MenuResolvers<ContextType>;
+  MenuItem?: MenuItemResolvers<ContextType>;
+  Page?: PageResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+};
+
